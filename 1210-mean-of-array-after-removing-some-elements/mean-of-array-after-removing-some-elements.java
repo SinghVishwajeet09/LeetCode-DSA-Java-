d3a5c8ed2a5d28acacf572df4acfab7509726eb5
Vishwajeet Raj Singh <singhvishwajeet958@gmail.com>
2026-06-14 16:27:10 +0530

    Time: 6 ms (86.17%) | Memory: 47.5 MB (19.36%) - LeetSync

---
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int removeCount = n / 20;
+        long sum = 0;
+        for (int i = removeCount; i < n - removeCount; i++) {
+            sum += arr[i];
+        }
+        return (double) sum / (n - 2 * removeCount);
+    }
+}
